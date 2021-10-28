f51e6b1e10a1f416df2eae2468bed84562ceca9a
jadonwellman <jadon.wellman@gmail.com>
2021-10-27 17:09:21 -0700

    Improved the graphing, although could still use some help with the panel plots

--- PA1.r
+++ PA1.r
@@ -1,5 +1,6 @@
 library(tidyr)
 library(dplyr)
+library(ggplot2)
 
 ## Load and preprocess the data
 
@@ -10,24 +11,26 @@ str(activity)
 
 ## What is mean total number of steps taken per day?
 
-steps_per_day <- activity %>% group_by(date) %>% summarize(sum(steps))
-steps_per_day <- rename(steps_per_day,steps = `sum(steps)`)
-attach(steps_per_day)
-hist(steps)
+steps_per_day <- activity %>% group_by(date) %>% summarize(sum(steps,rm.na=TRUE))
+steps_per_day <- rename(steps_per_day,steps_sum = `sum(steps, rm.na = TRUE)`)
+ggplot(steps_per_day,aes(x=steps_sum)) + geom_histogram() + 
+  labs(title='Histogram of Total Steps Per Day',x='Steps',y='Frequency Count')
+
 print(paste('Median total steps per day:',median(steps, 
   na.rm = TRUE)))
-print(paste('Mean total steps per day:',mean(steps, 
-  na.rm = TRUE)))
+print(paste('Mean total steps per day:',as.integer(mean(steps, 
+  na.rm = TRUE))))
 
 ## What is the average daily activity pattern?
 
 steps_per_interval <- activity %>% group_by(interval) %>% summarize(mean(steps,
   na.rm = TRUE))
 steps_per_interval <- rename(steps_per_interval,
-                             steps = `mean(steps, na.rm = TRUE)`)
-attach(steps_per_interval)
-plot(x=interval,y=steps,type = 'l')
-max_interval <- steps_per_interval[steps == max(steps),]$interval
+                             steps_mean = `mean(steps, na.rm = TRUE)`)
+ggplot(steps_per_interval,aes(x=interval,y=steps_mean)) + geom_line() + 
+  labs(title='Average Daiy Activity',x='Interval',y='Step Mean')
+max_interval <- steps_per_interval[steps_per_interval$steps_mean == 
+                                     max(steps_per_interval$steps_mean),]$interval
 print(paste('Interval with most steps on average:',max_interval))
 
 ## Imputing missing values
@@ -39,14 +42,16 @@ activity_cleansed <- activity %>% group_by(interval) %>% mutate(steps =
   replace_na(steps, mean(steps, na.rm = TRUE)))
 steps_per_day_cleansed <- activity_cleansed %>% group_by(date) %>% 
   summarize(sum(steps))
-steps_per_day_cleansed <- rename(steps_per_day_cleansed,steps = `sum(steps)`)
+steps_per_day_cleansed <- rename(steps_per_day_cleansed,steps_sum = `sum(steps)`)
 
-attach(steps_per_day_cleansed)
-hist(steps)
+ggplot(steps_per_day_cleansed,aes(x=steps_sum)) + geom_histogram() + 
+  labs(title='Histogram of Total Steps Per Day, Cleansed',x='Steps',
+       y='Frequency Count')
+
 print(paste('Median total steps per day:',
-  median(steps, na.rm = TRUE)))
+  as.integer(median(steps_per_day_cleansed$steps_sum, na.rm = TRUE))))
 print(paste('Mean total steps per day:',
-  mean(steps, na.rm = TRUE)))
+  as.integer(mean(steps_per_day_cleansed$steps_sum, na.rm = TRUE))))
 
 ## Are there differences in activity patterns between weekdays and weekends?
 
